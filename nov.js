4a84ba367a1a934339883d09f64e34b87326d8e6
Maryam Ibikunle <ibikunlemaryamibidoyin@gmail.com>
2025-11-14 21:18:17 +0100

    message

--- nov.js
+++ nov.js
@@ -1,10 +1,10 @@
-// const createCircle = (radius) =>{
-//     return {
-//         radius: radius,
-//         diameter: 2*radius,
-//         area : function(){
-//             return (22/7)*radius**2
-//         },
+const createCircle = (radius) =>{
+    return {
+        radius: radius,
+        diameter: 2*radius,
+        area : function(){
+            return (22/7)*radius**2
+        },
 //         perimeter: function() {
 //             return 2*(22/7)*radius
 //         }
