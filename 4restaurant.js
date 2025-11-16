38218674ecb41f1210ebe261e207cc215e3326ca
Maryam Ibikunle <ibikunlemaryamibidoyin@gmail.com>
2025-11-16 21:21:56 +0100

    cleared restaurant slution

--- 4restaurant.js
+++
@@ -1,5 +0,0 @@
-const restaurant = [
-    {name: "restaurant a", opened: "0800", closed: "1600"},
-    {name: "restaurant b", opened: "0700", closed: "1700"},
-    {name: "restaurant b", opened: "0600", closed: "1800"}
-]
